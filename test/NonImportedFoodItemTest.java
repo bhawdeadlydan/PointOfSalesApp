573c23ced904885d9c864bc76b904e71874103ef
Bhawesh Kumar Singh <bhawdeadlydan@gmail.com>
2015-06-09 16:24:56 +0530

    Bhawesh - Spec for import duty on non imported food item

--- test/NonImportedFoodItemTest.java
+++ test/NonImportedFoodItemTest.java
@@ -3,9 +3,7 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
-
-public class ImportedFoodItemTest {
-
+public class NonImportedFoodItemTest {
     @Test
     public void shouldCalculateSalesTax() {
         ImportedFoodItem chocolate = new ImportedFoodItem(50.00);
@@ -15,12 +13,5 @@ public class ImportedFoodItemTest {
         assertThat(salesTax, is(5.00));
     }
 
-    @Test
-    public void shouldCalculateImportDutyTax() {
-        ImportedFoodItem chocolate = new ImportedFoodItem(50.00);
 
-        double importDuty = chocolate.importDuty();
-
-        assertThat(importDuty, is(2.75));
-    }
 }
